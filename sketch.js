7d18837ce8e00ab13e61ae291a7e5664d736569b
Dipti Malik <malik.dipti@gmail.com>
2021-05-07 14:59:45 +0530

    Sketch changed

--- sketch.js
+++ sketch.js
@@ -40,6 +40,8 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+
+    text("Dipti", 50,50)
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
